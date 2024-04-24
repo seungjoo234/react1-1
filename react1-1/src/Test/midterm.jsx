925719a94f8f91c019bbc1b7cc4679a9c9003aa0
cage1004 <seungjoo234@naver.com>
2024-04-24 11:40:10 +0900

    2번째 커밋

--- react1-1/src/Test/midterm.jsx
+++ react1-1/src/Test/midterm.jsx
@@ -1,1 +1,38 @@
 import React from "react";
+
+export default class Eaxmple extends React.Component {
+    
+constructor(props) {
+    super(props);
+
+    this.state = {
+      text: "당신은 어떤 과일을 좋아하나요?",
+      FruitsText: "",
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.text} {this.state.FruitsText}</h1>
+        <button onClick={this.Apple}>사과</button>
+        <button onClick={this.Orange}>오렌지</button>
+        <button onClick={this.Banana}>바나나</button>
+      </div>
+    );
+    }
+    Apple = () => {
+        this.setState({
+            FruitsText: "사과",
+        });
+      };
+    Orange = () => {
+        this.setState({
+            FruitsText: "오렌지",
+        });
+    };
+    Banana = () => {
+        this.setState({
+            FruitsText: "바나나",
+        });
+    };
+  }
